2996c86a009e25a638003166be33ca724a41ea45
João Gabriel <jgmoreira15@gmail.com>
2020-04-04 12:22:24 -0300

    Criação e mapeamento das entidades Empresa e RamoAtividade

---
+++ src/main/java/com/jgmonteiro/erp/model/TipoEmpresa.java
@@ -0,0 +1,19 @@
+package com.jgmonteiro.erp.model;
+
+public enum TipoEmpresa {
+	
+	MEI("Microempreendedor Individual"),
+	EIRELI("Empresa Individual de Responsabilidade Limitada"),
+	LTDA("Sociedade Limitada"),
+	SA("Sociedade Anônima");
+	
+	private String descricao;
+	
+	TipoEmpresa(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
